0c2cdedccf101899788b9b0cdeb084795627e84f
Matt Smida <mcs@m4t7.com>
2023-12-05 21:06:17 -0500

    initial structure of bernies bikes db -- cc exit at this time

--- bikes.sql
+++ bikes.sql
@@ -3,3 +3,22 @@ CREATE TABLE customers (
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL
 );
+
+
+CREATE TABLE orders(
+    order_id SERIAL PRIMARY KEY,
+    code VARCHAR(50) REFERENCES bikes,
+    price NUMERIC(6, 2) NOT NULL,
+    customer_id INT REFERENCES customers,
+    comments TEXT,
+    purchase_time TIMESTAMP WITH TIMEZONE
+);
+
+CREATE TABLE bikes(
+    code VARCHAR(50) PRIMARY KEY
+    price NUMERIC(6, 2),
+    model VARCHAR(50)
+)
+
+
+
